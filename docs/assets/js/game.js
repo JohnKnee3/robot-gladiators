1225af5d9f099485f1836930cd13ab5b30175ebf
john.a.clark3 <john.a.clark3@gmail.com>
2021-12-20 17:34:32 -0700

    Fought some robuts
    Changed the trigger to a for loop and cleaned up the top of the script

--- docs/assets/js/game.js
+++ docs/assets/js/game.js
@@ -11,26 +11,12 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames);
-//I think this is note for these guys
-console.log(enemyNames[0])
-console.log(enemyNames[1])
-console.log(enemyNames[2])
-console.log(enemyNames.length)
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -85,5 +71,6 @@ var fight = function() {
   }
 };
 
-// // run fight function to start game
-// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
